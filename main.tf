920f42a25cd17863f7fc028d40ca64982c5f45ca
daniel bastos <daniel.cgid@gmail.com>
2020-03-08 11:40:17 -0300

    basic instance

--- main.tf
+++ main.tf
@@ -9,3 +9,13 @@ resource "aws_instance" "dev" {
     key_name = 
   
 }
+provider "aws" {
+    version = "~> 2.0"
+    region  = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-07ebfd5b3428b6f4"
+    aws_instance = "t2.micro"
+    key_name = terraform
+}
