e98b1d4750e9d07c83e79cd26fe4d0031e254de6
daniel bastos <daniel.cgid@gmail.com>
2020-03-08 11:47:28 -0300

    fix key

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "dev" {
     count = 3
     ami = "ami-07ebfd5b3428b6f4"
     aws_instance = "t2.micro"
-    key_name = terraform
+    key_name = "terraform"
     tags = {
         Name = "dev${count.index}"
     }
