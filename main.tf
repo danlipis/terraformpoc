20d81245dc0580e120877d81d193b7843089ae5b
daniel bastos <daniel.cgid@gmail.com>
2020-03-08 12:02:06 -0300

    fix

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "dev" {
     count = 3
-    ami = "ami-07ebfd5b3428b6f4"
+    ami = "ami-07ebfd5b3428b6f4d"
     instance_type = "t2.micro"
     key_name = "terraform"
     tags = {
