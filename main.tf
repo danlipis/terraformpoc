ffe6742e804113d1bd8b473afdba09bdf3db2180
daniel bastos <daniel.cgid@gmail.com>
2020-03-08 12:19:19 -0300

    wip

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us_east-1"
     access_key = "AKIAZ3B5HSP3TFJYQ6UV"
-    secrety_key = "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"
+    secret_key = "55QDSMQeufio0NTebGUxO91deazn++aoaL2F82dw"
     version = "~> 2.0"
 }
 
