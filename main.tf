f1d98f60b8ddb77707e6a20be3bdcbedd35a9ceb
daniel bastos <daniel.cgid@gmail.com>
2020-03-08 11:41:40 -0300

    fix duplicate

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
 provider "aws" {
     version = "~> 2.0"
-    region  = "us-east-1"  
-}
-
-resource "aws_instance" "dev" {
-    ami = "ami-08bc77a2c7eb2b1da"
-    instance_type = "t2.micro"
-    key_name = 
-  
-}
-provider "aws" {
-    version = "~> 2.0"
     region  = "us-east-1"
 }
 
